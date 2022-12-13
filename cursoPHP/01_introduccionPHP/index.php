7340f6f5f6643ed3aeb5d2c322c8c7f56670356e
Optimus-alfaomega <a-and-o-technologies@outlook.com>
2022-12-13 10:25:48 -0500

    first commit

---
+++ cursoPHP/01_introduccionPHP/index.php
@@ -0,0 +1,25 @@
+<?php 
+print "Mi primer codigo";
+#variable numerica
+$numeroCamel = 3;
+echo $numeroCamel;
+$colores= array("rojo","amarillo");
+echo $colores[0];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="#" > referencia </a>
+    <?php  
+        print "codigo interno";
+    ?>
+</body>
+</html>
